d2e611c50aff928681ede3e3413bc633dc16d29f
tpisfil <tpisfil13@gmail.com>
2021-07-14 16:23:42 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard lastname="Doe" firstname="Jane" age="45" hair="black"/>
-      <PersonCard lastname="Smith" firstname="John" age="88" hair="brown"/>
-      <PersonCard lastname="Fillmore" firstname="Millard" age="50" hair="brown"/>
-      <PersonCard lastname="Smith" firstname="Maria" age="62" hair="brown"/>
+      <PersonCard lastname="Doe" firstname="Jane" age={45} hair="black"/>
+      <PersonCard lastname="Smith" firstname="John" age={88} hair="brown"/>
+      <PersonCard lastname="Fillmore" firstname="Millard" age={50} hair="brown"/>
+      <PersonCard lastname="Smith" firstname="Maria" age={62} hair="brown"/>
     </div>
   );
 }
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.age
+        };
+    }
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastname}, {this.props.firstname}</h1>
-                <h4>Age: {this.props.age}</h4>
+                <h4>Age: {this.state.age}</h4>
                 <h4>Hair Color: {this.props.hair}</h4>
+                <button onClick={ ()=> { this.setState({age: this.state.age +1}) } }>Birthday Button for {this.props.firstname}!</button>
             </div>
         );
     }
